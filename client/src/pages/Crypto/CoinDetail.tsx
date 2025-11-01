0e7fe016f82779059eb2f55be9b69425f3af73bf
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-11-01 17:45:29 -0300

    feat: 2 pages for 'Crypto' system

---
+++ client/src/pages/Crypto/CoinDetail.tsx
@@ -0,0 +1,11 @@
+const CoinDetail: React.FC = () => {
+
+    return (
+        <div>
+            <h1>CoinDetail</h1>
+        </div>
+    )
+
+}
+
+export default CoinDetail
---
+++ client/src/pages/Crypto/Crypto.tsx
@@ -0,0 +1,11 @@
+const Crypto: React.FC = () => {
+
+    return (
+        <div>
+            <h1>Crypto</h1>
+        </div>
+    )
+
+}
+
+export default Crypto
