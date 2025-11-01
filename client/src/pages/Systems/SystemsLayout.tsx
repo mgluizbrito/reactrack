a22558b3df4e5e41d7f9e1a7e221d9a84ba0972f
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-11-01 17:38:32 -0300

    feat: layout for systems

--- client/src/pages/Systems/SystemsLayout.tsx
+++ client/src/pages/Systems/SystemsLayout.tsx
@@ -1,13 +1,20 @@
+import { Outlet, useLocation } from "react-router-dom"
 import Div from "../../components/Html/Div/Div"
 import NavbarSystems from "../../components/NavbarSystems/NavbarSystems"
 import HeaderSystems from "../../components/HeaderSystems/HeaderSystems"
 
 const SystemsLayout: React.FC = () => {
 
+    const location = useLocation()
+    const isSystemRoute = location.pathname !== '/systems' && location.pathname.startsWith('/systems/')
+
     return (
-        <Div className='flex flex-col items-center justify-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center'>
+        <Div className={`flex flex-col items-center min-h-screen bg-[url("/bg_img.png")] bg-cover bg-center ${!isSystemRoute ? 'justify-center' : ''}`}>
             <NavbarSystems />
-            <HeaderSystems />
+            {!isSystemRoute && <HeaderSystems />}
+            <Div className={isSystemRoute ? 'w-full pt-16' : ''}>
+                <Outlet />
+            </Div>
         </Div>
     )
 
