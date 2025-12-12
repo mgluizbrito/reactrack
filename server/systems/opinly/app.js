26df869d32a722bf4a98461681e15dfc7a223e2e
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-12-11 21:40:59 -0300

    feat: server, db and dependencies for 'Opinly' backend

---
+++ server/systems/opinly/app.js
@@ -0,0 +1,23 @@
+import express from "express"
+
+import { middleware } from "./middleware/httpRequests.js"
+import opinionsRouter from "./routes/opinions.js"
+
+const app = express()
+
+app.use(middleware)
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+    next()
+})
+
+app.use(express.json())
+
+app.use(opinionsRouter)
+
+app.listen(3010, () => {
+    console.log(`Backend do Opinly pronto para depositar suas opiniões...`)
+})
