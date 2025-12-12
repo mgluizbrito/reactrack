46af56adaee18581db01fba352406107208f6ee7
guilhermesilva <guilhermeviniciusbispodasilva@gmail.com>
2025-12-11 21:38:26 -0300

    feat: components for 'Crypto' page

--- client/src/pages/Crypto/CoinDetail.tsx
+++ client/src/pages/Crypto/CoinDetail.tsx
@@ -1,10 +1,77 @@
+import { useContext, useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { CoinContext } from "../../context/Coin/CoinContext"
+import classes from "./CoinDetail.module.css"
+import { fetchData } from "../../utils/systems/fit"
+import { getCoin, getDays, getMarketChart } from "../../api/urls/coin"
+import type { IPricesCoinData, ISpecificCoinData } from "../../interfaces/systems/crypto"
+import { options } from "../../api/urls/geral"
+import LineChart from "../../components/Systems/Crypto/common/LineChart"
+import Div from "../../components/Html/Div/Div"
+import Navbar from "../../components/Systems/Crypto/partials/Navbar"
+
 const CoinDetail: React.FC = () => {
 
-    return (
-        <div>
-            <h1>CoinDetail</h1>
-        </div>
-    )
+    const { id } = useParams()
+    const [coinData, setCoinData] = useState<ISpecificCoinData>()
+    const [historicalData, setHistoricalData] = useState<IPricesCoinData>()
+
+    const coinContext = useContext(CoinContext)
+    const currency = coinContext?.currency
+
+    const fetchCoinData = async () => {
+        const data = await fetchData(`${getCoin}${id}`, options);
+        setCoinData(data)
+    }
+
+    const fetchHistoricalData = async () => {
+        const data = await fetchData(`${getCoin}${id}/${getMarketChart}${currency?.name}&${getDays}`, options)
+        setHistoricalData(data)
+    }
+
+    useEffect(() => {
+        fetchCoinData()
+        fetchHistoricalData()
+    }, [currency])
+
+    if (coinData && historicalData) {
+        return (
+            <Div className={classes.coin}>
+                <Navbar />
+                <Div className={classes['coin-name']}>
+                    <img src={coinData.image.large} alt="Moeda" />
+                    <p><strong>{coinData.name} ({coinData.symbol.toUpperCase()})</strong></p>
+                </Div>
+                <Div className={classes['coin-chart']}>
+                    <LineChart historicalData={historicalData} />
+                </Div>
+                <Div className={classes['coin-info']}>
+                    <ul>
+                        <li>Rank no Mercado de Criptomoedas</li>
+                        <li>{coinData.market_cap_rank}</li>
+                    </ul>
+                    <ul>
+                        <li>Preço Atual</li>
+                        <li>{currency?.symbol} {coinData.market_data.current_price[currency?.name || 'usd']?.toLocaleString()}</li>
+                    </ul>
+                    <ul>
+                        <li>Maior Preço em 24h</li>
+                        <li>{currency?.symbol} {coinData.market_data.high_24h[currency?.name || 'usd']?.toLocaleString()}</li>
+                    </ul>
+                    <ul>
+                        <li>Menor Preço em 24h</li>
+                        <li>{currency?.symbol} {coinData.market_data.low_24h[currency?.name || 'usd']?.toLocaleString()}</li>
+                    </ul>
+                </Div>
+            </Div>
+        )
+    } else {
+        return (
+            <div className={classes.spinner}>
+                <div className={classes.spin}></div>
+            </div>
+        )
+    }
 
 }
 
--- client/src/pages/Crypto/Crypto.tsx
+++ client/src/pages/Crypto/Crypto.tsx
@@ -1,9 +1,88 @@
+import React, { useContext, useEffect, useState } from "react"
+import classes from "./Crypto.module.css"
+import { CoinContext } from "../../context/Coin/CoinContext"
+import { Link } from "react-router-dom"
+import type { ICoinData } from "../../interfaces/systems/crypto"
+import Div from "../../components/Html/Div/Div"
+import Header1 from "../../components/Html/Header1/Header1"
+import Paragraph from "../../components/Html/Paragraph/Paragraph"
+import Navbar from "../../components/Systems/Crypto/partials/Navbar"
+
 const Crypto: React.FC = () => {
 
+    const coinContext = useContext(CoinContext)
+
+    const allCoin = coinContext?.allCoin;
+    const currency = coinContext?.currency;
+
+    const [displayCoin, setDisplayCoin] = useState<ICoinData[]>([])
+
+    const [input, setInput] = useState('')
+
+    const inputHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInput((event.target.value).toLowerCase())
+        if (event.target.value === "") {
+            setDisplayCoin(allCoin ?? [])
+        }
+    }
+
+    const searchHandler = (event: React.FormEvent) => {
+        event.preventDefault()
+        const coins = allCoin?.filter((item: ICoinData) => {
+            return item.name.toLowerCase().includes(input)
+        })
+        setDisplayCoin(coins ?? [])
+    }
+
+    useEffect(() => {
+        setDisplayCoin(allCoin ?? [])
+    }, [allCoin])
+
     return (
-        <div>
-            <h1>Crypto</h1>
-        </div>
+        <Div className={classes.home}>
+            <Navbar />
+            <Div className={classes.hero}>
+                <Header1 text="Mercado de Criptomoeda" />
+                <Paragraph text="Bem-vindo ao Mercado de Criptomoeda. Explore todas as criptomoedas!" />
+                <form onSubmit={searchHandler}>
+                    <input onChange={inputHandler} list="coinlist" value={input} type="text" placeholder="Procure pelas criptomoedas..." required />
+
+                    <datalist id="coinlist">
+                        {allCoin?.map((item: ICoinData, index: number) => (
+                            <option key={index} value={item.name} />
+                        ))}
+                    </datalist>
+
+                    <button type="submit">Procurar</button>
+                </form>
+            </Div>
+
+            <Div className={classes['crypto-table']}>
+                <Div className={classes['table-layout']}>
+                    <p>#</p>
+                    <p>Moedas</p>
+                    <p>Preço</p>
+                    <p style={{textAlign: 'center'}}>24H Change</p>
+                    <p className={classes['market-cap']}>Market Cap</p>
+                </Div>
+                {
+                    displayCoin.slice(0, 10).map((item: ICoinData, index: number) => (
+                        <Link to={`/systems/crypto/coin/${item.id}`} className={classes['table-layout']} key={index}>
+                            <p>{item.market_cap_rank}</p>
+                            <Div>
+                                <img src={item.image} alt="Logo" />
+                                <p>{item.name + ' - ' + item.symbol}</p>
+                            </Div>
+                            <p>{currency?.symbol} {item.current_price.toLocaleString()}</p>
+                            <p className={item.price_change_percentage_24h > 0 ? classes.green : classes.red}>
+                                {Math.floor(item.price_change_percentage_24h * 100)/100}
+                            </p>
+                            <p className={classes['market-cap']}>{currency?.symbol} {item.market_cap.toLocaleString()}</p>
+                        </Link> 
+                    ))
+                }
+            </Div>
+        </Div>
     )
 
 }
